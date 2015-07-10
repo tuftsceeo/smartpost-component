cb2c0f55c2e88b164016df61fa2a10a8d4834561
Rafi <ryagudin@gmail.com>
2015-07-10 15:23:50 -0600

    sp_cat comment updates

--- mycomponent/sp_catMyComponent.php
+++ mycomponent/sp_catMyComponent.php
@@ -2,15 +2,36 @@
 if (!class_exists("sp_catMyComponent")) {
 
     /**
-     * Extends sp_catComponent
-     * Content category component. Defines administrative features
-     * for the content component. Also used alongside sp_postComponent
-     * for front-end handling.
+     * The first steps in creating a SmartPost component are to define your "sp_cat" and
+     * "sp_post" classes. Each class serves a different purpose. The classes prefixed with "sp_cat"
+     * are associated with the administrative dashboard interface, the classes prefixed with "sp_post"
+     * correspond to rendering components on the posts themselves, i.e. on the "front-end" of the site.
+     *
+     * Note that both the "sp_cat" and "sp_post" classes inherit from abstract classes: "sp_catComponent" and
+     * "sp_postComponent" respectively. I highly recommend to go into these abstract classes and look at what they do;
+     * a lot of the "behind scenes" of SmartPost happens inside these classes.
      *
      * @see sp_catComponent
      */
     class sp_catMyComponent extends sp_catComponent{
 
+
+        /**
+         * The constructor is used both for creating a new component and loading an existing one. If a
+         * component ID is passed in to the constructor, then it will attempt to load that component.
+         *
+         * @param int $compID  - The component ID, if a value > 0 is given, then it will attempt to load that component)
+         * @param int $catID   - The category associated with the component
+         * @param string $name - The component name
+         * @param string $description - The component description
+         * @param int $typeID    - The type ID of the component
+         * @param int $order     - The order at which the component appears in the template
+         * @param null $options  - Options that can be saved and retrieved from the database
+         * @param bool $default  - This flag decides if the component is rendered by default or not when the template is
+         *                         generated on the front-end.
+         * @param bool $required - Whether the component is required to be filled in by the user prior to submitting.
+         *                         Also prohibits the user from deleting the component.
+         */
         function __construct($compID = 0, $catID = 0, $name = '',
                              $description = '', $typeID = 0, $order = 0,
                              $options = null, $default = false, $required = false){
@@ -22,6 +43,13 @@ if (!class_exists("sp_catMyComponent")) {
         }
 
         /**
+         * The method gets called when SmartPost is first activated and makes SmartPost aware of the name of the
+         * component, its description, and where it resides.
+         *
+         * Note: The name of the component should not have any spaces in it.
+         *
+         * This method is inherited from the abstract "sp_catComponent" class.
+         *
          * @see parent::installComponent()
          */
         function install(){
@@ -29,12 +57,17 @@ if (!class_exists("sp_catMyComponent")) {
         }
 
         /**
+         * In future versions, this method will be called when SmartPost is uninstalled. Right now it doesn't do
+         * anything.
+         * @see sp_uninstall class
          * @see parent::uninstall()
          */
         function uninstall(){}
 
         /**
-         * Adds CSS / JS to stylize and handle any UI actions
+         * Used to initialize the component, I use this method to enqueue any styles or JavaScript libraries that
+         * the component depends on. I split up these actions into the "equeueCSS" and "enqueueJS" methods, but that
+         * is optional.
          */
         static function init(){
             self::enqueueCSS();
@@ -44,10 +77,26 @@ if (!class_exists("sp_catMyComponent")) {
         /**
          * Add content component JS
          */
-
         static function enqueueJS(){
-            wp_register_script( 'sp_catMyComponentJS', plugins_url('/js/sp_catMyComponent.js', __FILE__));
-            //wp_enqueue_script( 'sp_catMyComponentJS', array('jquery', 'sp_admin_globals', 'sp_admin_js', 'sp_catComponentJS') );
+
+            /**
+             * Here is where you can register any JS libraries associated with the "admin" portion of your component.
+             * Note that I prefixed the JS file with "sp_cat" to denote that it is associated with administrative
+             * actions.
+             *
+             * The third parameter in 'wp_register_script' defines JS library dependencies. SmartPost has three JS
+             * dependencies that every JS library should depend on:
+             *  1) jquery
+             *  2) sp_admin_globals - Global SmartPost JS vars
+             *  3) sp_admin_js - Admin-side vars and methods
+             *  4) sp_catComponentJS - JS vars and methods that are available to be used for new components
+             */
+            wp_register_script(
+                'sp_catMyComponentJS',
+                plugins_url('/js/sp_catMyComponent.js', __FILE__),
+                array('jquery', 'sp_admin_globals', 'sp_admin_js', 'sp_catComponentJS')
+            );
+            wp_enqueue_script( 'sp_catMyComponentJS' );
         }
 
         /**
